e5e282880defb647f31dee13c315d01b6c941c96
r3czus <76210775+r3czus@users.noreply.github.com>
2022-09-06 21:12:17 +0200

    .
    ..

---
+++ bindowanie dalej/cos.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace bindowanie_dalej
+{
+    /// <summary>
+    /// Logika interakcji dla klasy cos.xaml
+    /// </summary>
+    public partial class cos : Window
+    {
+        public cos()
+        {
+            InitializeComponent();
+        }
+    }
+}
