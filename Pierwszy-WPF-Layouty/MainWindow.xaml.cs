a807a92a282f8277508e6a89cb1ee4ebc7419c8a
r3czus <76210775+r3czus@users.noreply.github.com>
2022-09-06 21:17:37 +0200

    .

---
+++ Pierwszy-WPF-Layouty/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pierwszy_WPF_Layouty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            moje_okno.Title = "Moja aplikacja";
+            Title = "inny tytuł";
+            //Działają polskie znaki w cudzysłowach I ZMIENNYCH :O
+        }
+    }
+}
